3d08286db0704b521410905307de5c98ffca573f
jmbeach <jaredbeachdesign@gmail.com>
2016-03-31 22:36:22 -0500

    fuck

---
+++ vendor/github.com/ricallinson/simplebdd/README.md
@@ -0,0 +1,49 @@
+# SimpleBDD Test Runner
+
+[![Build Status](https://travis-ci.org/ricallinson/simplebdd.svg?branch=master)](https://travis-ci.org/ricallinson/simplebdd)
+
+Super simple behavior-driven development style test writer for Go.
+
+    package something
+
+    import(
+        "testing"
+        . "github.com/ricallinson/simplebdd"
+    )
+
+    func TestSomething(t *testing.T) {
+        Describe("AssertEqual()", func() {
+            It("should return that true is true", func() {
+                AssertEqual(true, true)
+            })
+            It("should return that false is not true", func() {
+                AssertNotEqual(false, true)
+            })
+            Skip("should return that 1 is not 2", func() {
+                AssertNotEqual(1, 2)
+            })
+        })
+        Report(t)
+    }
+
+## Xunit Reporting
+
+If you want to output Junit style reports, call `Xunit()` just prior to `Report()` passing the given `*testing.T` variable. This will create a folder named _test-results_ that contains an __xml__ file for each executed `Describe` block.
+
+    func TestSomething(t *testing.T) {
+        Describe("AssertEqual()", func() {
+            It("should return that true is true", func() {
+                AssertEqual(true, true)
+            })
+        })
+        Xunit(t)
+        Report(t)
+    }
+
+## Testing
+
+    go test -cover
+
+## Coverage
+    
+    go test -coverprofile=coverage.out; go tool cover -html=coverage.out -o=index.html
---
+++ vendor/github.com/ricallinson/simplebdd/xunit.go
@@ -0,0 +1,61 @@
+package simplebdd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+)
+
+var TEST_DIR = "test-results"
+var FILE_MASK os.FileMode = 0777
+
+type testResult struct {
+	name      string
+	classname string
+	time      float32
+}
+
+func writeXunitFile(name string, time float32, tests []testResult, errors int, skipped int, failures int) {
+	xml := "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+	xml += fmt.Sprintf("<testsuite name=\"%s\" time=\"%f\" tests=\"%d\" errors=\"%d\" skipped=\"%d\" failures=\"%d\">\n", name, time, len(tests), errors, skipped, failures)
+	for _, test := range tests {
+		xml += fmt.Sprintf("\t<testcase name=\"%s\" classname=\"%s\" time=\"%f\"/>\n", test.name, test.classname, test.time)
+	}
+	xml += "</testsuite>\n"
+	ioutil.WriteFile("./"+TEST_DIR+"/"+strings.Replace(name, " ", "_", -1)+".xml", []byte(xml), FILE_MASK)
+}
+
+func Xunit(t *testing.T) {
+
+	group := ""
+	tests := []testResult{}
+	passed := 0
+	failed := 0
+	skipped := 0
+
+	os.Mkdir("./"+TEST_DIR, FILE_MASK)
+
+	for _, test := range testRun.tests {
+		if group != test.group {
+			if group != "" {
+				writeXunitFile(group, 0.0, tests, 0, skipped, failed)
+			}
+			group = test.group
+			tests = []testResult{}
+			passed = 0
+			failed = 0
+			skipped = 0
+		}
+		if test.skipped {
+			skipped++
+		} else if test.passed {
+			passed++
+		} else {
+			failed++
+		}
+		tests = append(tests, testResult{test.group, test.message, 0.0})
+	}
+	writeXunitFile(group, 0.0, tests, 0, skipped, failed)
+}
